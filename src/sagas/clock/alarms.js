483ce7db97c07ed2da0bc99eabe54cb22d9d6979
Sergei Savoniuk <sergsavoniuk@gmail.com>
2019-04-19 17:08:23 +0300

    Alarm subapp - finished implementing saga logic

--- src/sagas/clock/alarms.js
+++ src/sagas/clock/alarms.js
@@ -1,88 +1,138 @@
-import { delay, race, put, take, select } from 'redux-saga/effects';
+import { delay, race, put, take, select, call } from 'redux-saga/effects';
 import { addDays, addMinutes } from 'date-fns';
 
 import { ActionTypes, ActionCreators, Selectors } from 'redux/clock/alarms';
 
+export function create(Class, ...args) {
+  return call(() => new Class(...args));
+}
+
 export default function* watchAlarms(alarmId) {
   while (true) {
     yield take(`${ActionTypes.SET_ALARM}.${alarmId}`);
-    const now = new Date();
-    const day = now.getDay() || 7;
-    const seconds =
-      now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds();
-    const alarmTime = yield select(Selectors.getAlarmTime, alarmId);
-    const sortedSelectedDays = (yield select(
-      Selectors.getSelectedDays,
-      alarmId,
-    )).sort((a, b) => a - b);
-    debugger;
 
-    let nextDayIndex = sortedSelectedDays.findIndex(
-      selectedDay => selectedDay >= day,
+    const startDate = yield create(Date);
+
+    const alarmTimeInMinutes = yield select(Selectors.getAlarmTime, alarmId);
+    const alarmDays = (yield select(Selectors.getSelectedDays, alarmId)).sort(
+      (a, b) => a - b,
     );
-    nextDayIndex = nextDayIndex > -1 ? nextDayIndex : 0;
-    let nextDay = sortedSelectedDays[nextDayIndex];
-    debugger;
-    let time;
 
-    if (nextDay === day) {
-      if (seconds > alarmTime * 60) {
-        nextDay = sortedSelectedDays[nextDayIndex + 1] || sortedSelectedDays[0];
-        nextDayIndex = sortedSelectedDays.findIndex(
-          selectedDay => selectedDay === nextDay,
-        );
+    const calculateTimeUntilTheNextAlarm = yield call(calculateTime, {
+      startDate,
+      alarmDays,
+      alarmTimeInMinutes,
+    });
 
-        if (nextDay > day) {
-          const diff = nextDay - day;
-          const newDate = addMinutes(
-            addDays(
-              new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-              diff,
-            ),
-            alarmTime,
-          );
-          time = newDate - now;
-        }
-      } else {
-        time = alarmTime * 60 - seconds;
-      }
-    } else if (nextDay > day) {
-      const diff = nextDay - day;
-      const newDate = addMinutes(
-        addDays(
-          new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-          diff,
-        ),
-        alarmTime,
-      );
-      time = newDate - now;
-    } else if (nextDay < day) {
-      const diff = 7 - day + nextDay;
-      const newDate = addMinutes(
-        addDays(
-          new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-          diff,
-        ),
-        alarmTime,
-      );
-      time = newDate - now;
-    }
+    let time = yield call(calculateTimeUntilTheNextAlarm);
 
-    // debugger;
     while (true) {
       const { alarmRang } = yield race({
-        // alarmRang: delay(time * 1000),
-        alarmRang: delay(10000000),
-
+        alarmRang: delay(time),
         removeAlarm: take(`${ActionTypes.SET_ALARM}.${alarmId}`),
       });
-      // debugger;
+
       if (alarmRang) {
         yield put(ActionCreators.setAlarmWentOff(alarmId, true));
-        break;
+        time = yield call(calculateTimeUntilTheNextAlarm);
       } else {
         break;
       }
     }
   }
+}
+
+// Helpers
+
+function calculateTime({ startDate, alarmDays, alarmTimeInMinutes }) {
+  const day = startDate.getDay() || 7;
+  let nextDay = day,
+    nextDayIndex = -1;
+
+  let time, differenceInDays, nextAlarmDate;
+
+  return function() {
+    let currentDay = nextDay;
+
+    [nextDay, nextDayIndex] = findNextDay({
+      alarmDays,
+      currentDay,
+      offset: nextDayIndex + 1,
+    });
+
+    startDate = nextAlarmDate || startDate;
+
+    if (nextDay === currentDay) {
+      if (timeHasAlreadyPassed(startDate, alarmTimeInMinutes)) {
+        [nextDay, nextDayIndex] = findNextDay({
+          alarmDays,
+          currentDay,
+          offset: nextDayIndex + 1,
+        });
+
+        if (nextDay > currentDay) {
+          differenceInDays = nextDay - currentDay;
+        } else {
+          differenceInDays = 7;
+        }
+      } else {
+        differenceInDays = time !== undefined ? 7 : 0;
+      }
+    } else if (nextDay > currentDay) {
+      differenceInDays = nextDay - currentDay;
+    } else if (nextDay < day) {
+      differenceInDays = 7 - currentDay + nextDay;
+    }
+
+    nextAlarmDate = calculateNextAlarmDate({
+      startDate,
+      differenceInDays,
+      alarmTimeInMinutes,
+    });
+
+    time = calculateTimeUntilTheNextAlarmInMillisecs(startDate, nextAlarmDate);
+
+    return time;
+  };
+}
+
+function timeHasAlreadyPassed(startDate, timeInMinutes) {
+  return startDate - buildDate(startDate).setMinutes(timeInMinutes) > 0;
+}
+
+function calculateNextAlarmDate({
+  startDate,
+  differenceInDays,
+  alarmTimeInMinutes,
+}) {
+  return addMinutes(
+    addDays(buildDate(startDate), differenceInDays),
+    alarmTimeInMinutes,
+  );
+}
+
+function calculateTimeUntilTheNextAlarmInMillisecs(startDate, nextAlarmDate) {
+  return nextAlarmDate - startDate;
+}
+
+function findNextDay(options) {
+  const nextDayIndex = findNextDayIndex(options);
+  return [options.alarmDays[nextDayIndex], nextDayIndex];
+}
+
+function findNextDayIndex({ alarmDays, currentDay, offset }) {
+  let nextDayIndex = -1;
+
+  for (let i = offset; i < alarmDays.length; ++i) {
+    if (alarmDays[i] >= currentDay) {
+      nextDayIndex = i;
+      break;
+    }
+  }
+
+  return nextDayIndex > -1 ? nextDayIndex : 0;
+}
+
+function buildDate(date) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
 }
